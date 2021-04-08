f677b61477bd0dacb848e44fa461909e43255598
ruwanganath ramanayake <ruwanganath@Samadi.localdomain>
2021-04-08 23:54:54 +1000

    Initialise spsf_dataanalysis

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express'),
     app = express();
 
-var port = process.env.PORT || 3000;   
+var port = process.env.PORT || 8081;   
 
 app.use(express.static(__dirname +'/public'));
 
 app.get('/',function(req,res){
-    res.send('Hello world-spsf')
+    res.send('Hello world-spsf_dataanalysis')
 })
 
 app.listen(port);
