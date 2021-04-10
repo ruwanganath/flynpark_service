3e41cabf750c1a72ae524f5748deffa121e897c5
ruwanganath ramanayake <ruwanganath@Samadi.localdomain>
2021-04-10 15:46:03 +1000

    spsf cont delivery test

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express'),
     app = express();
+const req = require('request');
 
 var port = process.env.PORT || 8081;   
 
 app.use(express.static(__dirname +'/public'));
 
-app.get('/',function(req,res){
-    res.send('Hello world-spsf_dataanalysis')
+app.get('/',function(request,response){
+    response.send('Hello world-spsf_dataanalysis')
 })
 
 app.listen(port);
