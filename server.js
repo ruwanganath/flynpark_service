68fed3e405323e90059de1478d650ef23929fc94
ruwanganath ramanayake <ruwanganath@Samadi.localdomain>
2021-04-25 20:25:00 +1000

    readme file changed

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const { json } = require('express');
 var express = require('express');
 app = express();
 
@@ -66,75 +65,6 @@ app.get('/generateParkingData',function (request,response){
                 })     
             }
         })
-    });
-
-    reqObject = "https://data.melbourne.vic.gov.au/resource/krh5-hhjn.json";
-    req(reqObject,(err,result,body)=> {
-        if(err){
-            return console.log(err);
-        }
-
-        data = JSON.parse(result.body)
-        arrayOffstreetData = convertJsonToArray(data)   
-
-        arrayOffstreetData.forEach((element) => {              
-            if(element.parking_type==='Commercial'){
-
-                arrayObject = {
-                    "type":'off',
-                    "bay": element.base_property_id,
-                    "lat": element.y_coordinate,
-                    "lon": element.x_coordinate_2,
-                    "desc1":element.parking_spaces,
-                    "desc2":""
-                }   
-                arrayData.push(arrayObject); 
-            }
-        })         
-    });
-
-    response.json(arrayData)
- })
-
-app.get('/generateParkingData',function (request,response){
-    //lat,lon,occupancy
-    reqObject = "https://data.melbourne.vic.gov.au/resource/vh2v-4nfs.json";
-    reqObject2 = "https://data.melbourne.vic.gov.au/resource/ntht-5rk7.json";
-    req(reqObject,(err,result,body)=> {
-        
-        if(err){
-            return console.log(err);
-        }
-        data = JSON.parse(result.body)
-        arrayOnstreetData = convertJsonToArray(data)    
-        
-        req(reqObject2,(err,result,body)=> {
-            if(err){
-                return console.log(err);
-            }
-            data2 = JSON.parse(result.body)
-            arrayOnstreetInfoData = convertJsonToArray(data2)
-         })
-       
-        arrayOnstreetData.forEach((element) => { 
-            if(element.status==='Unoccupied'){
-                
-                arrayOnstreetInfoData.forEach((element2) => { 
-                    if(element.bay_id === element2.bayid){
-                        arrayObject = {
-                            "type":'on',
-                            "bay": element.bay_id,
-                            "lat": element.lat,
-                            "lon": element.lon,
-                            "desc1":element2.description1,
-                            "desc2":element2.description2
-                        }   
-                        arrayData.push(arrayObject);  
-                    }
-                })     
-            }
-        })
-        
     });
 
     reqObject = "https://data.melbourne.vic.gov.au/resource/krh5-hhjn.json";
